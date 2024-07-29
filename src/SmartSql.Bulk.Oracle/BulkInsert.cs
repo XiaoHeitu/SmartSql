48fec96574a6234739b70b11a7f2d1a215b28c0b
yuanwj <ywj6792341@qq.com>
2024-07-29 17:01:56 +0800

    创建项目

---
+++ src/SmartSql.Bulk.Oracle/BulkInsert.cs
@@ -0,0 +1,33 @@
+﻿using SmartSql.DbSession;
+using System;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Data;
+
+namespace SmartSql.Bulk.Oracle
+{
+    public class BulkInsert : AbstractBulkInsert
+    {
+        public BulkInsert(IDbSession dbSession) : base(dbSession)
+        {
+
+        }
+
+        public override void Insert()
+        {
+            DbSession.Open();
+            InsertImpl();
+        }
+
+        private void InsertImpl()
+        {
+            
+        }
+
+        public override async Task InsertAsync()
+        {
+            await DbSession.OpenAsync();
+            InsertImpl();
+        }
+    }
+}
